057b21efa77997ae66b2202dbf87020069b4366f
cyris258 <nar941502@gmail.com>
2025-03-03 17:41:56 +0800

    Site updated: 2025-03-03 17:41:56

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1740994671507";
+const VERSION = "1740994907318";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
